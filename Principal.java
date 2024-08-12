35ca27c03ef4a95424d712628c90ce6c46aa9280
facundoOconell <166182870+facundoOconell@users.noreply.github.com>
2024-08-12 19:19:45 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import javax.swing.JFrame;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+    Ventana ventana = new Ventana();
+    ventana.setVisible(true);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
